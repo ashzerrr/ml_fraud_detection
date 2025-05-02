0ea3683f2d88fb537d977789abbaf11310120ed5
ashzerrr <ashtonjosh0014@gmail.com>
2025-05-01 21:49:48 -0400

    Initial commit: ML fraud detection project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Detecting synthetic credit card fraud using machine learning models to identify suspicious transaction patterns and reduce financial risk.',
+    author='Ashton_Kabou',
+    license='',
+)
