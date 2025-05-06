65f37501e16c3add331328c6f00218af6a6fd6c9
Ashton Kabou <129709857+ashzerrr@users.noreply.github.com>
2025-05-05 22:07:36 -0400

    Update load_data.py

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import json
 
-def load_data():
-    transactions_df = pd.read_csv("data/raw/transactions_data.csv")
-    cards_df = pd.read_csv("data/raw/cards_data.csv")
-    users_df = pd.read_csv("data/raw/users_data.csv")
-    with open("data/raw/train_fraud_labels.json", "r") as f:
-        fraud_labels = json.load(f)
-    return transactions_df, cards_df, users_df, fraud_labels
+def load_datasets():
+    transactions_df = pd.read_csv("transactions_data.csv")
+    cards_df = pd.read_csv("cards_data.csv")
+    users_df = pd.read_csv("users_data.csv")
+
+    with open("mcc_codes.json", "r") as f:
+        mcc_codes_df = json.load(f)
+    with open("train_fraud_labels.json", "r") as f:
+        train_fraud_labels_df = json.load(f)
+
+    train_fraud_labels_df = pd.DataFrame(list(train_fraud_labels_df["target"].items()), columns=['transaction_id', 'Fraud label'])
+    train_fraud_labels_df['transaction_id'] = train_fraud_labels_df['transaction_id'].astype(int)
+
+    return transactions_df, cards_df, users_df, mcc_codes_df, train_fraud_labels_df
